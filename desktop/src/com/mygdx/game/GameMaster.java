bdeead8d66c316300f801d8a3e1e0fa3a1d885fa
zhiyi-laptop <>
2024-01-26 17:20:03 +0800

    first
    first

---
+++ desktop/src/com/mygdx/game/GameMaster.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class GameMaster extends ApplicationAdapter{
+	
+	@Override
+	public void create() {
+		
+	}
+	
+	@Override
+	public void render() {
+		System.out.println("before");
+	}
+}
