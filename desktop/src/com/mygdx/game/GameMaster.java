95593bd69391c455f858e0d7482d0efd1cca3a1e
ivanlow249 <145116787+ivanlow249@users.noreply.github.com>
2024-02-09 11:40:47 +0800

    Updated GameMaster.java

--- desktop/src/com/mygdx/game/GameMaster.java
+++ desktop/src/com/mygdx/game/GameMaster.java
@@ -1,16 +1,44 @@
 package com.mygdx.game;
-
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 
 public class GameMaster extends ApplicationAdapter{
-	
-	@Override
-	public void create() {
-		
-	}
-	
-	@Override
-	public void render() {
-		System.out.println("test");
-	}
+	private Screen screen1;
+   
+    private Screen currentScreen;
+
+    @Override
+    public void create() {
+        screen1 = new MainMenuScreen();
+        //screen2 = new PauseScreen();
+        currentScreen = screen1;
+        currentScreen.show();
+    }
+
+    @Override
+    public void render() {
+        float delta = Gdx.graphics.getDeltaTime();
+        currentScreen.render(delta);
+    }
+
+    @Override
+    public void dispose() {
+        screen1.dispose();
+//        screen2.dispose();
+    }
+
+    public void changeScreen(int screenNumber) {
+//        if (screenNumber == 1) {
+//            currentScreen.hide();
+//            currentScreen = screen1;
+//            currentScreen.show();
+//        } 
+//        else if (screenNumber == 2) {
+//            currentScreen.hide();
+//            currentScreen = screen2;
+//            currentScreen.show();
+//        }
+    }
 }
+
