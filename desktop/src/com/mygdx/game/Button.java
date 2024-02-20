9206b394108ac5b15884d772329722b1ed9d34d0
ivanlow249 <145116787+ivanlow249@users.noreply.github.com>
2024-02-20 16:10:39 +0800

    Update Button.java

--- desktop/src/com/mygdx/game/Button.java
+++ desktop/src/com/mygdx/game/Button.java
@@ -1,7 +1,6 @@
 package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.graphics.Color;
@@ -17,35 +16,40 @@ public class Button {
 	 private float posX;
 	 private float posY;
 	 
+	 // Create a constructor that takes in 4 arguments
 	 public Button(float x, float y, float width, float height) {
 		 this.posX= x;
 		 this.posY= y;
 		 this.bounds = new Rectangle(x, y, width, height);
 		 this.font = new BitmapFont();
 	 }
+	 
 	 public void setColour(Color colour)
 	{
 			this.color= colour;
 	}
-	 public void setText(String Text)
-	{
-			this.text = Text;
-			
-	}
-	 public boolean isClicked() {
-	        return bounds.contains(this.posX, this.posY);
+	 
+	 public void setText(String text) {
+		    this.text = text != null ? text : ""; // Ensure text is not null
+		}
+	 
+	 public boolean hover(float touchX, float touchY) {
+		 return bounds.contains(touchX, touchY);
 	 }
+	
 	 public void render(ShapeRenderer shapeRenderer, SpriteBatch batch) {
 		 // Draw the button rectangle
 		 shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		 shapeRenderer.setColor(this.color);
+		 shapeRenderer.setColor(color);
 		 shapeRenderer.rect(bounds.x, bounds.y, bounds.width, bounds.height);
 		 shapeRenderer.end();
 
 		 // Draw the text inside the button
 		 batch.begin();
+		 BitmapFont font = new BitmapFont();
 		 font.setColor(Color.BLACK);
 		 font.getData().setScale(2);
+//		 font.draw(batch, text, this.posX, bounds.y + bounds.height / 2 , bounds.width, Align.center, true);
 		 font.draw(batch, text, bounds.x, bounds.y + bounds.height / 2, bounds.width, Align.center, true);
 		 batch.end();
 	 }
