ea2318f9edd7580825ebacc8d2ad58163be8657b
ivanlow249 <145116787+ivanlow249@users.noreply.github.com>
2024-03-26 18:21:58 +0800

    modified switchTo()
    else if (screen instanceof GameScreen) 
            {
            	entityManager.playWordSound();
            	
            	hud.setWorldTimer(300);
                currentScreen = gameScreen;
            }

--- desktop/src/com/mygdx/game/ScreenManager.java
+++ desktop/src/com/mygdx/game/ScreenManager.java
@@ -1,95 +1,257 @@
-package com.mygdx.game;
+package GameEngine.Entity.Screen;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
+import GameLayer.HUD;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import GameEngine.Entity.EntityManager;
+import GameEngine.Entity.OrthographicCameraController;
 
 public class ScreenManager {
-	
-	private MainMenuScreen mainMenuScreen;
-	private InstructionsScreen instructionsScreen;
-	private PauseScreen pauseScreen;
-	private GameScreen gameScreen;
-	private GameOverScreen gameOverScreen;
-	// private GameMaster gameScreen;
-	private Screen currentScreen;
 
-	
-    private ArrayList<Screen> screens;
+    private final HUD hud;
+    private Stage stage;
     
+    private MainMenuScreen mainMenuScreen;
+    private InstructionsScreen instructionsScreen;
+    private PauseScreen pauseScreen;
+    private GameScreen gameScreen;
+    private GameOverScreen gameOverScreen;
+    private Scene currentScreen;
+    private EntityManager entityManager;
+    private OrthographicCameraController orthographicCameraController;
+    private World world;
 
-    public ScreenManager() {
-        screens = new ArrayList<>();
-        mainMenuScreen = new MainMenuScreen(); // Initialize mainMenuScreen
-        instructionsScreen = new InstructionsScreen(); // Initialize instructionsScreen
-        gameScreen = new GameScreen(); // Initialize gameScreen
-        pauseScreen = new PauseScreen(this, gameScreen); // Initialize pauseScreen with appropriate parameters
-        gameOverScreen = new GameOverScreen(); // Initialize gameOverScreen
+    private ShapeRenderer shapeRenderer;
+    private BitmapFont font;
+    private Music backgroundMusic;
+    private ArrayList<Scene> screensList;
+	private Random rand = new Random();
+
+    // Create a constructor
+    public ScreenManager(EntityManager entityManager, World world, OrthographicCameraController orthographicCameraController, SpriteBatch batch) {
+    	setEntityManager(entityManager);
+        setWorld(world);
+        setCamera(orthographicCameraController);
+        
+        hud = new HUD(new Stage(new ScreenViewport()), 300, entityManager.getPCLives()); // Create HUD instance
+    	stage = hud.getStage();
+
+        screensList = new ArrayList<Scene>();
+        shapeRenderer = new ShapeRenderer();
+        font = new BitmapFont();
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("themeSong.mp3"));
+
+        // Create an object of mainMenuScreen, add to the screensList
+        mainMenuScreen = new MainMenuScreen(batch, shapeRenderer, font, 170, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        screensList.add(mainMenuScreen);
+
+        // Create an object of instructionsScreen, add to the screensList
+        instructionsScreen = new InstructionsScreen(batch, shapeRenderer, font, 100, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        screensList.add(instructionsScreen);
+
+        // Create an object of gameScreen, add to the screensList
+        gameScreen = new GameScreen(stage, batch, shapeRenderer, font, 50, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), hud);
+        screensList.add(gameScreen);
+
+        // Create an object of pauseScreen, add to the screensList
+        pauseScreen = new PauseScreen(this, gameScreen, batch, shapeRenderer, font, 110, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        screensList.add(pauseScreen);
+
+        // Create an object of gameOverScreen, add to the screensList
+        gameOverScreen = new GameOverScreen(batch, shapeRenderer, font, 110, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        screensList.add(gameOverScreen);     
+
+        setScreenManager(this);
     }
 
 
-    public void addScreen(Screen screen) {
-        screens.add(screen);
-    }
+    // This function will always be called to load screen into the game
+    public void drawCurrent(float delta) {
+        // currentScreen will call getCurrentScreen() to return a Screen
+        currentScreen = getCurrentScreen();
 
-    public void removeScreen(Screen screen) {
-        screens.remove(screen);
-    }
-    
-    public void setCurrentScreen(Screen screen) {
-    	currentScreen = screen;
-    }
-    
-    public void switchTo(Screen screen) {
-    	if (screen instanceof PauseScreen) {
-            currentScreen = pauseScreen;
-        } else if (screen instanceof GameScreen) {
-        	System.out.println(currentScreen);
-        	currentScreen.hide();
-            currentScreen = gameScreen;
+        // If currentScreen has nothing assigned, set the current screen to MainMenuScreen
+        if (currentScreen == null) {
+            setCurrentScreen("Main");
+        }
+        // If currentScreen has been assigned with a screen, then show the screen
+        else {
+        	Gdx.gl.glClearColor(0, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            ScreenUtils.clear(0, 0, 0.2f, 1);
+            
             currentScreen.show();
-        } else if (screen instanceof InstructionsScreen) {
-            currentScreen = instructionsScreen;
-        } else if (screen instanceof MainMenuScreen) {
+            currentScreen.render(delta, currentScreen.getBatch(), currentScreen.getShape(), currentScreen.getMapFont());
+
+            // Check if the current screen is the game screen
+            boolean isGameScreen = currentScreen instanceof GameScreen;
+
+            // Update HUD visibility based on whether it's the game screen or not
+            hud.setVisible(isGameScreen);
+
+
+            // If it's the game screen, render the HUD
+            if (isGameScreen) {
+                hud.render(entityManager.getPCLives());
+//                System.out.println(hud.getWorldTimer());
+            }
+        }
+    }
+    
+    public void resize(int width, int height) {
+    	orthographicCameraController.resize(width, height);
+		hud.resize(width, height); // Resize the HUD
+
+//		int viewportWidth = stage.getViewport().getScreenWidth();
+//		int viewportHeight = stage.getViewport().getScreenHeight();
+//		System.out.println("Viewport width: " + viewportWidth + ", height: " + viewportHeight);
+
+		hud.getStage().getViewport().update(width, height, true);
+    }
+    
+    // Checking for game state
+    public void checkGameStart(float MAP_SCALE) {
+        backgroundMusic.setLooping(true); // Set the music to loop
+        backgroundMusic.setVolume(0.05f);
+        backgroundMusic.play();
+
+        if (getCurrentScreen() instanceof GameScreen) {
+            gameState(getCurrentScreen().getBatch(), world);
+        }
+
+		hud.render(entityManager.getPCLives());
+		stage.act();
+		stage.draw();
+    }
+
+    public void setScreenManager(ScreenManager screenManagerInput) {
+        mainMenuScreen.setScreenManager(screenManagerInput);
+        instructionsScreen.setScreenManager(screenManagerInput);
+        gameScreen.setScreenManager(screenManagerInput);
+        pauseScreen.setScreenManager(screenManagerInput);
+        gameOverScreen.setScreenManager(screenManagerInput);
+    }
+
+    public void setEntityManager(EntityManager entityManagerInput) {
+        entityManager = entityManagerInput;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setCamera(OrthographicCameraController orthographicCameraControllerInput) {
+        orthographicCameraController = orthographicCameraControllerInput;
+    }
+
+    public OrthographicCameraController getCamera() {
+        return orthographicCameraController;
+    }
+
+    public void setWorld(World worldInput) {
+        world = worldInput;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+    public HUD getHUD() {
+    	return hud;
+    }
+
+    // Access to screensList
+    public void addScreen(Scene screen) {
+        screensList.add(screen);
+    }
+
+    public void removeScreen(Scene screen) {
+        screensList.remove(screen);
+    }
+
+    // This function returns a Scene type
+    public Scene getCurrentScreen() {
+        return currentScreen;
+    }
+
+    public void setCurrentScreen(String screenString) {
+        // Loop through each Screen in screenList
+        for (Scene screen : screensList) {
+            // There is an abstract method in Scene class called getScreen() that returns a String type
+            if (screen.getScreen() == screenString) {
+                // If the screenString is in the screenList, assign it to currentScreen
+                switchTo(screen);
+            }
+        }
+    }
+
+    // Switching between screens
+    public void switchTo(Scene screen) {
+        if (screen instanceof MainMenuScreen) {
             currentScreen = mainMenuScreen;
+        } 
+        else if (screen instanceof GameScreen) 
+        {
+        	entityManager.playWordSound();
+        	
+        	hud.setWorldTimer(300);
+            currentScreen = gameScreen;
+        } 
+        else if (screen instanceof GameOverScreen) {
+        	entityManager.getPC("PlayableCharacter").setGuess(null);
+            currentScreen = gameOverScreen;
+        } 
+        else if (screen instanceof InstructionsScreen) {
+            currentScreen = instructionsScreen;
+        } 
+        else if (screen instanceof PauseScreen) {
+            currentScreen = pauseScreen;
+        }
+    }
+
+    public void gameState(SpriteBatch batch, World world) {
+        // Update Box2d Objects outside of World Simulation
+    	entityManager.count();
+        if (entityManager.getNum() > 0) {
+            update();
         } else {
-            // Handle other types of screens
-        }
-    }
-    
-    /*
-    public void switchToGameScreen() {
-        currentScreen.dispose(); // Dispose current screen if necessary
-        gameScreen = new GameMaster(this); // Reinitialize GameScreen
-        currentScreen = gameScreen; // Set current screen to GameScreen
-    }
-    
-    public void switchTo(Screen screen) {    	
-    	// if instruction button is clicked, go to Instruction Screen
-    	
-    	// if start button is clicked, go to Game Screen
-    	
-    	// if pause button is clicked, go to Pause Screen 
-    	
-    	// after collision occurs, show Game Over Screen
-    	
-    	// if exit button is clicked, exit the game
-    }
-    
+            entityManager.entityDraw(batch);
+            entityManager.movement(orthographicCameraController.getMapFullWidth());
 
-    public void drawScreens() {
-        for (Screen screen : screens) {
-            screen.update();
+            world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+
+            if (entityManager.getPC("PlayableCharacter") != null && entityManager.getPCLives() > 0) {
+                orthographicCameraController.camera(batch);
+            } else {
+                setCurrentScreen("GameOver");
+                update();
+            }
+            if(entityManager.getPC("PlayableCharacter").checkWin()) {
+            	setCurrentScreen("GameOver");
+                update();
+            }
         }
     }
 
-    public Screen getCurrentScreen() {
+    public void update() {
+        entityManager.entityCollision(world);
+    }
 
-	
-    } 
-    */
+    public void screenDispose() {
+        font.dispose();
+        shapeRenderer.dispose();
+        backgroundMusic.dispose();
+        hud.dispose(); // Dispose the HUD
+    }
 }
